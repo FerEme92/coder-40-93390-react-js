4ea63bf16a0427d43a4148dd69f8d29a2c506032
Ffernando Medrano <medranoefe@gmail.com>
2026-05-20 23:11:50 -0300

    primerCommit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+//import de componentes 
+// import + nombre del componente + from+ direccion del arch
+import Navbar from './componentes/Navbar'
+
+
+function App(){
+  return (
+    <h1>Hola Chicos</h1>
+  );
+};
+
+export default App;
---
+++ src/componentes/Navbar.jsx
@@ -0,0 +1,16 @@
+//import css
+import "../css/Navbar.css"
+
+const Navbar= ()=>{
+
+    return(
+        <nav className ='nav-container'> 
+            <a href="">Coder App</a>
+            <a href="">Nuevos</a>
+            <a href="">Ofertas</a>
+            <a href="">Mas Vendidos</a>
+        </nav>
+    )
+}
+
+export default Navbar
